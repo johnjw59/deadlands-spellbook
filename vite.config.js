e874799aa0d18b30cde2fcf08c9658bd4c9647ac
John Wiebe <johnjw59@gmail.com>
2026-06-27 05:59:18 -0700

    Initial scaffold

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.png'],
+      manifest: {
+        name: 'Deadlands Weird West Spellbook',
+        short_name: 'DeadlandsSpellbook',
+        description: 'Spellbook and character tracker for Savage Worlds Adventure Edition: Deadlands',
+        theme_color: '#D5AA52',
+        background_color: '#81652f',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+    }),
+  ],
+});
